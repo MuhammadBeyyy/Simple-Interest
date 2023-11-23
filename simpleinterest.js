af604d08805df9c0653663258498aaffa4a92e2d
MuhammadBeyyy <146830245+MuhammadBeyyy@users.noreply.github.com>
2023-11-23 06:27:49 -0800

    Update simpleinterest.js

--- simpleinterest.js
+++ simpleinterest.js
@@ -1,1 +1,16 @@
+//Simple Intererst
+document.getElementById('calculate-button').addEventListener('click' , function()
+ {
+    const principal = parseFloat(document.getElementById('principal').value)
+    const rate = parseFloat(document.getElementById('rate').value)
+    const time =parseFloat(document.getElementById('time').value)
 
+
+if (isNaN(principal) || isNaN(rate) || isNaN(time)) {
+    alert('PLEASE ENTER VALID NUMBERS FOR ALL FIELDS.')
+}
+
+const interest = (principal * rate * time) / 100
+
+document.getElementById('result').innerText =`The simple interst is :$${interest.toFixed(2)}`
+})
